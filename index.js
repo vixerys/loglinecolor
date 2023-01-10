64afa0e39bcf2c65bca275c4fc93be4b243980cc
callmeceris <76592623+vixerys@users.noreply.github.com>
2023-01-11 06:16:54 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,97 @@
+class loglinecolor {
+    constructor() { }
+
+    /**
+     * black color
+     * 
+     * @param {string} params 
+     * @returns {string}
+     */
+    black(params) {
+        return console.log("\x1b[30m" + params + "\x1b[0m");
+    }
+
+    /**
+     * red color
+     * 
+     * @param {string} params 
+     * @returns {string}
+     */
+    red(params) {
+        return console.log("\x1b[31m" + params + "\x1b[0m");
+    }
+
+    /**
+     * green color
+     * 
+     * @param {string} params 
+     * @returns {string}
+     */
+    green(params) {
+        return console.log("\x1b[32m" + params + "\x1b[0m");
+    }
+
+    /**
+     * yellow color
+     * 
+     * @param {string} params 
+     * @returns {string}
+     */
+    yellow(params) {
+        return console.log("\x1b[33m" + params + "\x1b[0m");
+    }
+    
+    /**
+     * blue color
+     * 
+     * @param {string} params 
+     * @returns {string}
+     */
+    blue(params) {
+        return console.log("\x1b[34m" + params + "\x1b[0m");
+    }
+
+    /**
+     * magenta color
+     * 
+     * @param {string} params 
+     * @returns {string}
+     */
+    magenta(params) {
+        return console.log("\x1b[35m" + params + "\x1b[0m");
+    }
+
+    /**
+     * cyan color
+     * 
+     * @param {string} params 
+     * @returns {string}
+     */
+    cyan(params) {
+        return console.log("\x1b[36m" + params + "\x1b[0m");
+    }
+
+    /**
+     * white color
+     * 
+     * @param {string} params 
+     * @returns {string}
+     */
+    white(params) {
+        return console.log("\x1b[37m" + params + "\x1b[0m");
+    }
+
+    /**
+     * gray color
+     * 
+     * @param {string} params 
+     * @returns {string}
+     */
+    gray(params) {
+        return console.log("\x1b[90m" + params + "\x1b[0m");
+    }
+}
+
+module.exports = { 
+    loglinecolor: loglinecolor,
+}
